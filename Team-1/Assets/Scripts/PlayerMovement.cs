a09dbe5ece8e9b06935ccf89f10667400eb02196
smallwanderer <99022143+smallwanderer@users.noreply.github.com>
2023-05-23 17:37:57 +0900

    main character movement
    캐릭터 장애물 피격 시 효과

--- Team-1/Assets/Scripts/PlayerMovement.cs
+++ Team-1/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
@@ -18,13 +19,15 @@ public class PlayerMovement : MonoBehaviour
     public Transform wallCheck;
     public float wallCheckDistance;
     public LayerMask wallLayer;
-    public int isRight=1;
+    public int isRight = 1;
     bool isWall;
-    public float wallingSpeed=0.2f;
+    public float wallingSpeed = 0.2f;
     public float walljumppower;
     public bool isWallJump;
+    bool isStunned = false;
+    public bool blockInput;
 
-    private enum MovementState { idle, running, jumping, falling, walling};
+    private enum MovementState { idle, running, jumping, falling, walling };
     MovementState state = MovementState.idle;
 
     // Start is called before the first frame update
@@ -37,85 +40,149 @@ public class PlayerMovement : MonoBehaviour
         wallLayer = GetComponent<LayerMask>();
     }
 
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+        {
+            isStunned = false;
+            blockInput = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "Box")
+        {
+            isStunned = true;
+            blockInput = false;
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+            Debug.Log("Box Collided!");
+            if (isRight == 1)
+            {
+                FlipPlayer();
+                rb.AddForce(new Vector2(isRight * -300, 100), ForceMode2D.Force);
+            }
+            else if (isRight == -1)
+            {
+                FlipPlayer();
+                rb.AddForce(new Vector2(isRight * -300, 100), ForceMode2D.Force);
+            }
+        }
+        Debug.Log("Collision is made:" + collision.gameObject.name);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
-        float dirX = Input.GetAxis("Horizontal");
-        if(!isWallJump)
-        rb.velocity = new Vector2(dirX*runSpeed, rb.velocity.y);
+        if (blockInput)
+        {
+            float dirX = Input.GetAxis("Horizontal");
+            if (!isWallJump)
+                rb.velocity = new Vector2(dirX * runSpeed, rb.velocity.y);
 
-        if (IsWall() && !(IsGrounded())) {
-            isWallJump = false;
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * wallingSpeed);
-            if (Input.GetButtonDown("Jump")) {
-                
-                CancelInvoke("FreezeX");
-                isWallJump = true;
-                Invoke("FreezeX", 1f);
+            if (IsWall() && !(IsGrounded()))
+            {
+                isWallJump = false;
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * wallingSpeed);
+                if (Input.GetButtonDown("Jump"))
+                {
 
-                if (isRight == 1) {
-                    FlipPlayer();
-                } else {
-                    FlipPlayer();
+                    CancelInvoke("FreezeX");
+                    isWallJump = true;
+                    Invoke("FreezeX", 1f);
+
+                    if (isRight == 1)
+                    {
+                        FlipPlayer();
+                    }
+                    else
+                    {
+                        FlipPlayer();
+                    }
+                    rb.velocity = new Vector2(isRight * walljumppower, 0.9f * walljumppower);
                 }
-                rb.velocity = new Vector2(isRight * walljumppower, 0.9f * walljumppower);
             }
+
+
+
+            if (Input.GetButtonDown("Jump") && IsGrounded())
+            {
+                rb.velocity = new Vector2(0, jumpPower);
+            }
+
+            UpdateAnimationUpdate(dirX);
         }
-
-
-
-        if (Input.GetButtonDown("Jump") && IsGrounded()) {
-            rb.velocity = new Vector2(0, jumpPower);
-        }
-
-        UpdateAnimationUpdate(dirX);
-        
-
-
     }
 
-    private bool IsGrounded() {
+    private bool IsGrounded()
+    {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumbleGround);
     }
-    private bool IsWall() {
+    private bool IsWall()
+    {
         return Physics2D.Raycast(wallCheck.position, Vector2.right * isRight, wallCheckDistance, wallLayer);
     }
-    void FreezeX() {
+    void FreezeX()
+    {
         isWallJump = false;
     }
-    void FlipPlayer() {
+    void FlipPlayer()
+    {
         transform.eulerAngles = new Vector3(0, Mathf.Abs(transform.eulerAngles.y - 180), 0);
         isRight = isRight * -1;
     }
 
-    private void UpdateAnimationUpdate(float dirX) {
+    private void UpdateAnimationUpdate(float dirX)
+    {
         MovementState state = MovementState.idle;
-        if (IsWall()) {
+        if (IsWall())
+        {
             //isWallJump = false;
             state = MovementState.walling;
-        } else {
-            if(!isWallJump)
-            if (dirX > 0f) {
-                state = MovementState.running;
-                if (isRight == -1) {
-                    FlipPlayer();
+        }
+        else
+        {
+            if (!isWallJump)
+                if (dirX > 0f)
+                {
+                    state = MovementState.running;
+                    if (isRight == -1)
+                    {
+                        FlipPlayer();
+                    }
                 }
-            } else if (dirX < 0f) {
-                state = MovementState.running;
-                if(isRight == 1) {
-                    FlipPlayer();
-                }    
-            } else {
-                state = MovementState.idle;
-            }
-            if (rb.velocity.y > .1f) {
+                else if (dirX < 0f)
+                {
+                    state = MovementState.running;
+                    if (isRight == 1)
+                    {
+                        FlipPlayer();
+                    }
+                }
+                else
+                {
+                    state = MovementState.idle;
+                }
+            if (rb.velocity.y > .1f)
+            {
                 state = MovementState.jumping;
-            } else if (rb.velocity.y < -.1f) {
+            }
+            else if (rb.velocity.y < -.1f)
+            {
                 state = MovementState.falling;
             }
         }
-        
+
         anim.SetInteger("state", (int)state);
+    }
+
+    void BlockInput()
+    {
+        blockInput = true;
+    }
+
+    void UnBlockInput()
+    {
+        blockInput = false;
     }
 }
